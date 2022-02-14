776b020b013b64d237ead6abe4b0231336242a43
EdgarasSp <dev.applications.esp@gmail.com>
2022-02-14 01:02:48 +0000

    Add main page options and delay import function

--- run.py
+++ run.py
@@ -1,6 +1,7 @@
 import os # to use clear terminal function
 import gspread
 from google.oauth2.service_account import Credentials
+from time import sleep # to add delay effect
 import random
 
 SCOPE = [
@@ -17,6 +18,10 @@ SHEET = GSPREAD_CLIENT.open('Tic-Tac-Toe-Data')
 game_data = SHEET.worksheet('game_data')
 data = game_data.get_all_values()
 
+
+# Variables
+
+delay = 0.3
 
 def clear_terminal():
     """
@@ -47,4 +52,46 @@ def logo():
                        \/_/\/___/  \/____/                                                 
   """)
 
-logo()
+def main_page():
+    """
+    Request player to confirm if play, view leaderboard or read instructions
+    """
+
+    clear_terminal()
+    logo()
+    sleep(delay*2)
+    print("                                    WELCOME\n")
+    sleep(delay*2)
+    print("                  Please choose from one of the options below:\n")
+    input_text = "                               [S] Start Game \n                               [L] Leaderboard \n                               [I] Instructions\n"
+    first = input(input_text)
+    valid_input = False
+
+    while not valid_input:
+        if first.lower() == "s":
+            valid_input = True
+
+        elif first.lower() == "l":
+            valid_input = True
+            leaderbords_page() #placeholder
+        
+        elif first.lower() == "i":
+            valid_input = True
+            instrunctions_page() #placeholder
+
+        else:
+            clear_terminal()
+            logo()
+            print("                                      UPS\n ")
+            print("           Not a valid input... your options are [s], [l] or [i] \n")
+            print("                          Returning to the Menu...\n ")
+            sleep(delay*6)
+            clear_terminal()
+            logo()
+            sleep(delay)
+            print("                                LET'S TRY AGAIN\n")
+            sleep(delay)
+            print("                  Please choose from one of the options below:\n")
+            first = input(input_text)
+
+main_page()
